571043947255fdb8c50e18c69bbc11c29496b609
JhoannS <sebastianzamudio2004@gmail.com>
2025-09-14 19:57:50 -0500

    Creacion de base inicial para las tablas

--- database/migrations/2025_09_15_005620_create_sessions_table.php
+++ database/migrations/2025_09_15_005620_create_sessions_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -12,10 +13,7 @@ return new class extends Migration {
     {
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->foreignId('user_id')
-                ->nullable()
-                ->constrained('usuarios')
-                ->nullOnDelete();
+            $table->foreignId('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
