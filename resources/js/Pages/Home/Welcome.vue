0150fc25643efc9ea9ac5e25c8b9075afe773c0f
Erik Guevara <emgladino@gmail.com>
2025-09-16 20:51:43 -0500

    Prueba de commit

--- resources/js/Pages/Home/Welcome.vue
+++ resources/js/Pages/Home/Welcome.vue
@@ -4,6 +4,10 @@ import { Head } from "@inertiajs/vue3";
 
 <template>
   <Head title="Bienvenido a F&C Consultores" />
-
+  <div>
+    <p>
+      cambios realizados por mi
+    </p>
+  </div>
  
 </template>
