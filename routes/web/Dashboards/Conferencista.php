28d5441855d3ebe307b673b88c74d4dca9f0fe4b
Er1K6U <emgladino@gmail.com>
2026-05-29 21:29:05 -0500

    fix: corrige dashboard de conferencista

--- routes/web/Dashboards/Conferencista.php
+++ routes/web/Dashboards/Conferencista.php
@@ -1,12 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Conferencistas\ConferencistaController;
 
 Route::middleware(['auth', 'verified'])->group(function () {
 
     Route::middleware(['rol:conferencista'])->prefix('speaker')->group(function () {
-        Route::get('/perfil', [SpeakerController::class, 'edit'])->name('speaker.dashboard');
+        Route::get('/perfil', [ConferencistaController::class, 'show'])->name('speaker.dashboard');
     });
 });
-
-   
